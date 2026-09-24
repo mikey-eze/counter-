382af4138a0e69855d11b06daaa164e94a488ad5
Lavani Mohammad Saif <215622460+mikey-eze@users.noreply.github.com>
2026-09-24 16:19:18 +0530

    Fix Java and Kotlin JVM targets

--- android/user/build.gradle.kts
+++ android/user/build.gradle.kts
@@ -6,6 +6,11 @@ plugins {
 android {
     namespace = "com.quizcounter.user"
     compileSdk = 35
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions { jvmTarget = "17" }
     defaultConfig {
         applicationId = "com.quizcounter.user"
         minSdk = 26
